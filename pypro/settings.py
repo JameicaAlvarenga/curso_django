d2a726542b5c3f58de20651c527e4325f045bc87
JameicaAlvarenga <jnbrag@gmail.com>
2021-04-13 17:04:03 -0300

    configurando heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qn$zvjcc#jiq!$mmmfxybia*^#rz!@e)%qdacd(+*tww-t!dww'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
