3b5b941f98ddcda9558b896ae1fcf287c7841915
JameicaAlvarenga <jnbrag@gmail.com>
2021-04-15 12:25:34 -0300

    configurando Banco de dados

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # new_django
+Aplicação disponivel em : https://pyprodjangojnbac.herokuapp.com/
 [![Build Status](https://travis-ci.com/JameicaAlvarenga/new_django.svg?branch=main)](https://travis-ci.com/JameicaAlvarenga/new_django)
 [![Updates](https://pyup.io/repos/github/JameicaAlvarenga/new_django/shield.svg)](https://pyup.io/repos/github/JameicaAlvarenga/new_django/)
 [![Python 3](https://pyup.io/repos/github/JameicaAlvarenga/new_django/python-3-shield.svg)](https://pyup.io/repos/github/JameicaAlvarenga/new_django/)
--- pypro/settings.py
+++ pypro/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from decouple import config
+from functools import partial
+
+import dj_database_url
+from decouple import config, Csv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qn$zvjcc#jiq!$mmmfxybia*^#rz!@e)%qdacd(+*tww-t!dww'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -73,11 +76,12 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+default_db_url= 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
 
