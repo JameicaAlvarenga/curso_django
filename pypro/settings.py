31ce84a3101a6f802653e8245f6ab38eac9e8935
JameicaAlvarenga <jnbrag@gmail.com>
2021-05-14 17:02:05 -0300

    teste debug toolbar

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ from functools import partial
 import os
 import dj_database_url
 from decouple import config, Csv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,3 +174,19 @@ if AWS_ACCESS_KEY_ID:
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
+
+SENTRY_DSN=config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(dsn="SENTRY_DSN", integrations=[DjangoIntegration()])
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+       # traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        #send_default_pii=True
+
+
