1f8a7463ee1a2d8bc4cc13edcc4eaa6d10f86d43
JameicaAlvarenga <jnbrag@gmail.com>
2021-06-03 19:29:02 -0300

    flake

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -12,9 +12,10 @@ def resp(client):
 def test_status_code(resp):
     assert resp.status_code == 200
 
+
 def test_titulo_video(resp):
-    assert_contains(resp,'<h1>Video Aperitivos: Motivação </h1>')
+    assert_contains(resp, '<h1>Video Aperitivos: Motivação </h1>')
 
 
 def test_conteudo_video(resp):
-    assert_contains(resp,'iframe width="560" height="315" src="https://www.youtube.com/embed/2aYplgJrPDU"')
+    assert_contains(resp, 'iframe width="560" height="315" src="https://www.youtube.com/embed/2aYplgJrPDU"')
