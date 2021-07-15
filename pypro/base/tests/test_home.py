f18245853a7b6de18e4eb1962a8a87a933cf2f9e
JameicaAlvarenga <jnbrag@gmail.com>
2021-07-15 18:57:53 -0300

    teste de video

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -13,9 +13,13 @@ def test_status_code(resp):
     assert resp.status_code == 200
 
 
+def test_title(resp):
+    assert_contains(resp, '<title>Python Pro - Home</title>')
+
+
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
 
 
 def test_email_link(resp):
-    assert_contains(resp, f'href={"mailto:ramalho@python.pro.br"}')
+    assert_contains(resp, 'href="mailto:ramalho@python.pro.br"')
